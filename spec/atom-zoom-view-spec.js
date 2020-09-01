bab659c46f845b1321c80a68c06d1c4277b8f3a9
magicdawn <magicdawn@qq.com>
2020-09-01 19:15:37 +0800

    chore: usable

---
+++ spec/atom-zoom-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomZoomView from '../lib/atom-zoom-view';
+
+describe('AtomZoomView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
